7ba46db4939f191a0bac925f7bd07a54d5c3f86b
Martin <mmaina290@gmail.com>
2025-01-21 19:22:20 +0300

    local run

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -24,12 +24,12 @@ export default function Footer() {
           <Typography color="grey.700" variant="h6">
             Developed by{" "}
             <Link
-              href="https://github.com/crazy-man22"
+              href="https://github.com/TintinSDev"
               underline="none"
               sx={{ color: "text.primary" }}
               target="_blank"
             >
-              Crazy Man
+              Tintin
             </Link>
           </Typography>
         </Divider>
